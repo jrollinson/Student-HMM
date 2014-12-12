9365e3c1141bbe75f23cd27706ebea1835b34e9e
Joseph Rollinson <JTRollinson@gmail.com>
2014-12-12 15:35:34 -0500

    Adds setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='hmm',
+      version='0.1',
+      description='A Hidden Markov Model Library',
+      author='Joseph Rollinson',
+      author_email='jtrollinson@gmail.com',
+      packages=['hmm'])
+
